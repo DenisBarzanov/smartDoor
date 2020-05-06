d437245e41540f62ac6c4a7e6843f9dbf7b58da2
DenisBarzanov <denis.barzanov2002@gmail.com>
2020-05-06 16:47:35 +0300

    Fix face recognising

--- face_recognising.py
+++ face_recognising.py
@@ -5,19 +5,58 @@ import numpy as np
 import pickle
 import RPi.GPIO as GPIO
 from time import sleep
+import signal
+import sys
 
-relay_pin = [26]
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0) 
+
+signal.signal(signal.SIGINT, signal_handler)
+
+power_pin = 19
+reverse_pin = [26]
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(relay_pin, GPIO.OUT)
-GPIO.output(relay_pin, 0)
+GPIO.setup(power_pin, GPIO.OUT)
+# GPIO.output(power_pin, 0)
+
+GPIO.setup(reverse_pin, GPIO.OUT)
+GPIO.output(reverse_pin, 0)
+
+p = GPIO.PWM(power_pin, 300); # 1000 Hz
+p.start(0);
+
+def unlockLock(): 
+    lockDoor();
+    sleep(1.3);
+    unlockDoor();
+def lockDoor():
+    p.ChangeDutyCycle(30.0);
+    #GPIO.output(power_pin, 1);
+    GPIO.output(reverse_pin, 1);
+    sleep(0.2);
+    p.ChangeDutyCycle(0);
+    #GPIO.output(power_pin, 0);
+    GPIO.output(reverse_pin, 0);
+
+def unlockDoor(): 
+    
+    p.ChangeDutyCycle(30.0);
+    #GPIO.output(power_pin, 1);
+    #GPIO.output(power_pin, 1);
+    GPIO.output(reverse_pin, 0);
+    sleep(0.2);
+    p.ChangeDutyCycle(0);
+    #GPIO.output(power_pin, 0);
 
 with open('labels', 'rb') as f:
-	dicti = pickle.load(f)
-	f.close()
+        dicti = pickle.load(f)
+        f.close()
 
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.framerate = 30
+camera.framerate = 3
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
 
@@ -28,33 +67,30 @@ recognizer.read("trainer.yml")
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	frame = frame.array
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
-	for (x, y, w, h) in faces:
-		roiGray = gray[y:y+h, x:x+w]
+        frame = frame.array
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+        print("new frame")
+        for (x, y, w, h) in faces:
+                roiGray = gray[y:y+h, x:x+w]
 
-		id_, conf = recognizer.predict(roiGray)
+                id_, conf = recognizer.predict(roiGray)
 
-		for name, value in dicti.items():
-			if value == id_:
-				print(name)
+                for name, value in dicti.items():
+                        if value == id_:
+                                print(name)
 
-		if conf <= 70:
-			GPIO.output(relay_pin, 1)
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
+                print(conf);
+                if conf >= 70:
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
+                        unlockLock();
+        cv2.imshow('frame', frame)
+        key = cv2.waitKey(1)
 
-		else:
-			GPIO.output(relay_pin, 0)
+        rawCapture.truncate(0)
 
-	cv2.imshow('frame', frame)
-	key = cv2.waitKey(1)
-
-	rawCapture.truncate(0)
-
-	if key == 27:
-		break
+        if key == 27:
+                break
 
 cv2.destroyAllWindows()
-
---
+++ run.sh
@@ -0,0 +1,3 @@
+Xvfb :99 &
+DISPLAY=:99
+python3 face_recognising.py
